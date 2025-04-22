d829754abd1252bc9038ae6f8635ae1d5fa345cd
Alexander Browne <elcste@users.noreply.github.com>
2025-04-22 14:37:36 -0500

    Change line-height and text-align to my preferences

--- reader.js
+++ reader.js
@@ -15,8 +15,8 @@ const getCSS = ({ spacing, justify, hyphenate }) => `
         }
     }
     p, li, blockquote, dd {
-        line-height: ${spacing};
-        text-align: ${justify ? 'justify' : 'start'};
+        line-height: 1.7;
+        text-align: unset;
         -webkit-hyphens: ${hyphenate ? 'auto' : 'manual'};
         hyphens: ${hyphenate ? 'auto' : 'manual'};
         -webkit-hyphenate-limit-before: 3;
